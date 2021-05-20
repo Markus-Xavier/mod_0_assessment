c053ec9218798e0f7a7d81e36d46530f7b640cd8
Markus Rossio <argitavis@gmail.com>
2021-05-19 22:37:30 -0500

    Add new files and modle a laptop

---
+++ models/modelA.js
@@ -0,0 +1,12 @@
+var lapTop = {
+    owner: 'Markus',
+    brand: 'Apple',
+    batteryPercentage: 100,
+    ageInYears: 0,
+    isPowered: true,
+    isClean: true,
+    attachments: ['Keyboard', 'Mouse', 'Monitor'],
+    hardware: ['Battery', 'CPU', 'RAM']
+};
+
+
