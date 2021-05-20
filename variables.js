0359f55892a4789605405252939d04e54f01aa49
Markus Rossio <argitavis@gmail.com>
2021-05-19 22:30:48 -0500

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,12 @@
+var animal = 'Rat';
+var isCuddly = true;
+var num8 = 8;
+var num3 = 3;
+
+var quotient = num3 / num8;
+
+var favoriteFilms = ['Harry Potter', 'The Hobbit', 'Warcraft'];
+
+favoriteFilms.shift();
+
+animal.length;
