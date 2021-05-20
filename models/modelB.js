36d68c40bf955330341fd1d352906afdcc0d799f
Markus Rossio <argitavis@gmail.com>
2021-05-19 22:42:05 -0500

    Add house model

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var house = {
+    homeOwner: 'Jerry',
+    streetName: 'Rat Road',
+    houseNumber: 1337,
+    yearBuild: 2020,
+    isForSale: false,
+    isForRent: true,
+    features: ['Garage', 'Fenced in backyard', 'Central Heating'],
+    availableShowingTimes: ['April 1st', 'April 9th', 'April 11th', 'April 12th']
+};
